04ce6797e7b553af0586440f2b019bd26d6fb241
Spencer E Olson <olsonse@umich.edu>
2012-02-25 23:00:45 -0700

    minor reformatting

--- gui/about.py
+++ gui/about.py
@@ -9,22 +9,22 @@ import gtk
 import version
 
 def show():
-    dialog = gtk.AboutDialog()
-    dialog.set_name(PROGNAME)
-    dialog.set_version( 'v'+version.version() )
-    dialog.set_authors(AUTHORS)
-    dialog.set_license(
-      'Arbitrary Waveform Generator:\n'
-      '  Timing and control program for data acquisition\n'
-      '   - Co-authored by Air Force Research Laboraory\n'
-      '     (U.S. Government)\n'
-      '\n'
-      'This software is released under the GPL v3 license'
-    )
-    dialog.set_wrap_license(80)
-    dialog.set_copyright("\302\251 Cold Atom Navigation Laboratory")
-    dialog.set_website("http://www.cold-atoms.afrl.af.mil./")
-    ## Close dialog on user response
-    dialog.connect ("response", lambda d, r: d.destroy())
-    dialog.show()
+  dialog = gtk.AboutDialog()
+  dialog.set_name(PROGNAME)
+  dialog.set_version( 'v'+version.version() )
+  dialog.set_authors(AUTHORS)
+  dialog.set_license(
+    'Arbitrary Waveform Generator:\n'
+    '  Timing and control program for data acquisition\n'
+    '   - Co-authored by Air Force Research Laboraory\n'
+    '     (U.S. Government)\n'
+    '\n'
+    'This software is released under the GPL v3 license'
+  )
+  dialog.set_wrap_license(80)
+  dialog.set_copyright("\302\251 Cold Atom Navigation Laboratory")
+  dialog.set_website("http://www.cold-atoms.afrl.af.mil./")
+  ## Close dialog on user response
+  dialog.connect("response", lambda d, r: d.destroy())
+  dialog.show()
 
