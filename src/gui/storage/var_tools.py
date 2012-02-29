4d15f9ed4f60015b3f4a3ca7b7d03d277bfdc053
Spencer E Olson <olsonse@umich.edu>
2012-02-29 07:59:26 -0700

    save configuration to file in a more pretty format

--- src/gui/storage/var_tools.py
+++ src/gui/storage/var_tools.py
@@ -1,12 +1,15 @@
 # vim: ts=2:sw=2:tw=80:nowrap
 
 import copy
+import pprint
 
 def writevars( F, vardict ):
   if not F:
     return
   for i in vardict.items():
-    F.write( '{K} = {V}\n'.format(K=str(i[0]),V=repr(i[1])) )
+    F.write( '{K} = \\\n'.format(K=str(i[0])) )
+    pprint.pprint(i[1], F )
+    F.write('\n')
 
 def readvars( F ):
   if not F:
