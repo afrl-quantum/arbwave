009dc1887dc42b16c9410de88441e9c47191e9f0
Spencer E Olson <olsonse@umich.edu>
2012-03-01 11:55:30 -0700

    more figuring out the driver interface

--- src/backend/drivers/__init__.py
+++ src/backend/drivers/__init__.py
@@ -5,22 +5,39 @@ Subdirectories of this part of the arbwave package should ONLY contain drivers.
 """
 
 import os
-analog = list()
-digital = list()
-timing = list()
-backplane = list()
+analog    = dict()
+digital   = dict()
+timing    = dict()
+backplane = dict()
+drivers   = dict() # mapping "prefix" to driver module
+
 
 def initialize_device_drivers():
-  global analog, digital, timing, backplane
+  global analog, digital, timing, backplane, drivers
   THISDIR = os.path.dirname( __file__ )
+
+  def add_with_prefix(P,L,C):
+    for c in C:
+      L[ P + '/' + str(c) ] = c
+
   for D in os.listdir(THISDIR):
     if os.path.isdir( os.path.join(THISDIR,D) ):
       try:
         m = __import__(D,globals=globals(),locals=locals())
-        analog    += m.get_analog_channels()
-        digital   += m.get_digital_channels()
-        timing    += m.get_arbitrary_timing_channels()
-        backplane += m.get_routeable_backplane_signals()
+        if m.prefix() in drivers:
+          raise NameError("Prefix of driver already used: '"+m.prefix()+"'")
+
+        drivers[m.prefix()] = m
+        ac = m.get_analog_channels()
+        dc = m.get_digital_channels()
+        tc = m.get_arbitrary_timing_channels()
+        rs = m.get_routeable_backplane_signals()
+
+        add_with_prefix( m.prefix(), analog,    ac )
+        add_with_prefix( m.prefix(), digital,   dc )
+        add_with_prefix( m.prefix(), timing,    tc )
+        add_with_prefix( m.prefix(), backplane, rs )
+
       except Exception, e:
         print "could not import backend '" + D + "'"
         print e
