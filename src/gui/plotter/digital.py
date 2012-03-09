559490a488ee9acc7931dfb852466192b3bdabfa
Spencer E Olson <olsonse@umich.edu>
2012-03-09 04:58:54 -0700

    change expected digital processor output
    The new format should simultaneously simplify use of the digital processor
    output by _all_ of the plotter, viewpoint DIO-64 cards, as well as digital _and_
    analog cards by National Instruments (and similar sample-based) cards.

--- src/gui/plotter/digital.py
+++ src/gui/plotter/digital.py
@@ -3,6 +3,7 @@
 Utilities for plotting digital signals
 """
 import numpy as np
+import copy
 
 # broken_barh( xranges, yrange, **kwargs )
 #  xranges : sequence of (xmin, xwidth)
@@ -28,39 +29,66 @@ def get_face_color( channel_number ):
 fc=get_face_color
 ls=get_linestyle
 
-def sigdict_to_list( sigs, grange, endpad ):
-  dt = np.append( np.diff( sigs[:,0] ), endpad )
-  return [ (sigs[i][0], dt[i])  for i in xrange(*grange)  if sigs[i][1] ]
+def get_t_final( signals ):
+  t_final = 0.0
+  for c in signals.values():
+    t_final = max( t_final, c.values()[-1][-1][0] )
+  return t_final
 
-def plot( ax, signals, end_padding=1 ):
+def mkdt( signal, t_final ):
+  dt = copy.deepcopy( signal )
+
+  #append the first item of the next grouping
+  items = signal.items()
+  for i in xrange(1,len(items)):
+    dt[ items[i-1][0] ].append( items[i][1][0] )
+  #append a pseudo-item for the last grouping time length
+  dt[ items[-1][0] ].append( (t_final, 1, None) )
+
+  for i in dt:
+    dt[i] = np.diff( np.array(dt[i])[:,0] )
+  return dt
+
+
+def mkbbars( L, dt ):
+  # only pick out the true value transitions
+  return [ (L[i][0], dt[i])   for i in xrange(len(L))  if L[i][2] ]
+
+
+def plot( ax, signals, end_padding=-1 ):
   """
     end_padding : how much to pad the end of the signal with in order to
-                  satisfy the demand that end-transitions be honored
+                  satisfy the demand that end-transitions be honored.
+                  If no padding is specified (or end_padding<0) 0.001 of total
+                  time will be used.
   """
-  channels = signals['channels'].items()
-  channels.sort( lambda (k1,v1),(k2,v2): cmp(k2,k1) ) # reverse lexical sort
-  dt = signals['dt']
+  #start by finding the maximum time
+  t_final = get_t_final( signals )
+  if end_padding < 0:
+    t_final *= 1.001
+  else:
+    t_final += end_padding
 
-  max_x = 0.0
+  channels = signals.items()
+  channels.sort( lambda (k1,v1),(k2,v2): cmp(k2,k1) ) # reverse lexical sort
+
   labels = list()
   i = 0
   for c in channels:
     labels.append( c[0] )
-    sigs = c[1]['signal'].items()
-    sigs.sort( lambda (k1,v1),(k2,v2): cmp(k1,k2) ) # sort numerically
-    for g in c[1]['groups'].items():
-      bbars = sigdict_to_list( np.array(sigs), g[0], dt[g[1]]*end_padding )
-      max_x = max( max_x, bbars[-1][0]+bbars[-1][1] )
+    dt = mkdt( c[1], t_final )
+
+    for g in c[1].items():
       ax.broken_barh(
-        bbars, (i,1),
-        facecolors=fc(i), linestyles=ls(g[1]), linewidth=2 )
+        mkbbars( g[1], dt[ g[0] ] ), (i,1),
+        facecolors=fc(i), linestyles=ls(g[0]), linewidth=2 )
     i += 1
 
   ax.set_xlabel('Time (s)')
   ax.set_yticks( np.r_[0.5:i] )
   ax.set_yticklabels(labels)
   ax.grid(True)
-  return max_x
+  return t_final
 
 
 # This should be conformant to the output that the arbwave.Processor produces.
@@ -68,45 +96,56 @@ def plot( ax, signals, end_padding=1 ):
 # in the following signals, it is expected that the last transition will be
 # honored and that generally the voltage will remain at the given level after
 # the signal is finished
+#
+# format:
+#  'channel-label' : {
+#    group-number : [(start-time, number-samples, value), ...],
+#    ...
+#  }
+#
+# Which pieces of the 3-tuple (start-time, number-samples, value) is
+# needed/ignored really depends on what is using this information.  For
+# plotting as well as hardware output with an Viewpoint DIO-64 cards, the
+# number-samples component will be ignored.  For digital output on cards like
+# National Instruments digital output card 6533(?), start-time will be ignored.
 example_signals = {
-  'dt' : { 0:.1, 1:.1, 2:.2, 3:.1 },
-  'channels' : {
-    'CH0' : {
-      'signal' :{0:True, 100:False, 110:True, 120:False, 130:True, 200:False, 220:True},
-      'groups' :{(0,4):0, (4,6):2, (6,7):3},
-    },
-    'CH1' : {
-      'signal' :{0:True, 50:False, 80:True, 100:False, 200:True, 210:False, 215:True, 220:False},
-      'groups' :{(0,4):0, (4,6):2, (6,8):3},
-    },
-    'CH2' : {
-      'signal' :{0:True, 50:False, 80:True, 100:False, 200:True, 210:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
-    'CH3' : {
-      'signal' :{10:True, 40:False, 90:True, 100:False, 200:True, 210:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
-    'CH4' : {
-      'signal' :{20:True, 60:False, 90:True, 110:False, 200:True, 210:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
-    'CH5' : {
-      'signal' :{0:True, 50:False, 80:True, 100:False, 200:True, 210:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
-    'CH6' : {
-      'signal' :{0:True, 100:False, 110:True, 120:False, 130:True, 200:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
-    'CH7' : {
-      'signal' :{0:True, 100:False, 110:True, 120:False, 130:True, 200:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
-    'CH8' : {
-      'signal' :{0:True, 100:False, 110:True, 120:False, 130:True, 200:False},
-      'groups' :{(0,4):0, (4,6):2},
-    },
+  'CH0' : {
+    0 : [(0,1000,True), (100,100,False), (110,100,True), (120,100,False)],
+    2 : [(130,35,True), (200,100,False)],
+    3 : [(220,1,True)],
+  },
+  'CH1' : {
+    0 : [(0,500,True), (50,300,False), (80,200,True), (100,1000,False)],
+    2 : [(200,50,True), (210,25,False)],
+    3 : [(215,50,True), (220,1,False)],
+  },
+  'CH2' : {
+    0 : [(0,500,True), (50,300,False), (80,200,True), (100,1000,False)],
+    2 : [(200,50,True), (210,1,False)],
+  },
+  'CH3' : {
+    0 : [(10,300,True), (40,500,False), (90,100,True), (100,1000,False)],
+    2 : [(200,50,True), (210,1,False)],
+  },
+  'CH4' : {
+    0 : [(20,400,True), (60,300,False), (90,100,True), (110,900,False)],
+    2 : [(200,50,True), (210,1,False)],
+  },
+  'CH5' : {
+    0 : [(0,500,True), (50,300,False), (80,200,True), (100,1000,False)],
+    2 : [(200,50,True), (210,1,False)],
+  },
+  'CH6' : {
+    0 : [(0,1000,True), (100,100,False), (110,100,True), (120,100,False)],
+    2 : [(130,350,True), (200,1,False)],
+  },
+  'CH7' : {
+    0 : [(0,1000,True), (100,100,False), (110,100,True), (120,100,False)],
+    2 : [(130,350,True), (200,1,False)],
+  },
+  'CH8' : {
+    0 : [(0,1000,True), (100,100,False), (110,100,True), (120,100,False)],
+    2 : [(130,350,True), (200,1,False)],
   },
 }
 
