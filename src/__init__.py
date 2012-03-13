57992faf208aea332047d8f8707746a10dd060ea
Spencer E Olson <olsonse@umich.edu>
2012-03-13 04:07:52 -0600

    add simple command-line handling

--- src/__init__.py
+++ src/__init__.py
@@ -3,11 +3,23 @@
 Arbitrary waveform generator for digital and analog signals.
 """
 
-import version
+import os, argparse, gtk
+import version, gui
 
 def main():
-  import gui
-  import gtk
-  gui.ArbWave()
+  parser = argparse.ArgumentParser(prog=version.prefix())
+  parser.add_argument( '--version', action='version',
+    version='%(prog)s '+version.version() )
+  parser.add_argument('filename', nargs='?', help='configuration file')
+  args = parser.parse_args()
+
+  prog = gui.ArbWave()
+  if args.filename:
+    assert os.path.isfile( args.filename ), 'expected configuration filename'
+    prog.config_file = args.filename
+    F = open( args.filename )
+    prog.setvars( gui.storage.var_tools.readvars(F) )
+    F.close()
+
   gtk.main()
 
