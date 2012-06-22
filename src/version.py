f5ee62cd67ebeb3eb7f2d5a4bc59d9727fa4cdc6
Spencer E Olson <olsonse@umich.edu>
2012-06-22 10:50:46 -0600

    add version save capability

--- src/version.py
+++ src/version.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
 # vim: ts=2:sw=2:tw=80:nowrap
 
 from subprocess import Popen, PIPE
+from os import path
+
+VERSION_FILE = path.join( path.dirname(__file__), 'VERSION' )
+UNKNOWN_VERSION = 'arbwave-UNKNOWN'
 
 def git_version():
   try:
@@ -8,12 +13,29 @@ def git_version():
     out,err = p.communicate()
     return out.strip()
   except:
-    return 'arbwave-UNKNOWN'
+    return UNKNOWN_VERSION
 
 def version():
   gv = git_version()
+  if gv == UNKNOWN_VERSION and path.isfile(VERSION_FILE):
+    f = open( VERSION_FILE )
+    gv = f.readline()
+    f.close()
+  if not gv:
+    gv = UNKNOWN_VERSION
   return gv[ (gv.find('-')+1):]
 
 def prefix():
   return git_version().split('-')[0]
 
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) > 1:
+    if sys.argv[1] == 'save':
+      gv = git_version()
+      f = open( VERSION_FILE, 'w' )
+      f.write(gv)
+      f.close()
+    elif sys.argv[1] == 'get':
+      print version()
