d6a215a0206710e3b352367500dbc651705742e0
Spencer E Olson <olsonse@umich.edu>
2012-07-30 10:40:16 -0600

    more debug info from simulated viewpoint driver

--- src/backend/drivers/viewpoint/sim.py
+++ src/backend/drivers/viewpoint/sim.py
@@ -9,28 +9,33 @@ class DIO64:
     self.out_len_mask = 0
 
   def DIO64_Open( self, board, ignored ):
-    pass
+    debug('DIO64_Open(%d,%s)', board, ignored)
 
   def DIO64_Load( self, board, rbfFile, num_inputs, num_outputs ):
-    pass
+    debug('DIO64_Load(%d,%s,%s,%s)', board, repr(rbfFile), num_inputs, num_outputs)
 
   def DIO64_Close( self, board ):
-    pass
+    debug('DIO64_Close(%d)', board)
 
   def DIO64_In_Start( self, board, ticks, mask, len_mask, ignored_flags, clock,
                       trig_type_edge, trig_source,
                       stop_type_edge, stop_source,
                       daq_clock_modulo, scan_rate ):
-    pass
+    debug('DIO64_In_Start(%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+          board, ticks, mask, len_mask, ignored_flags, clock,
+          trig_type_edge, trig_source, stop_type_edge, stop_source,
+          daq_clock_modulo, scan_rate )
 
   def DIO64_In_Status( self, board, scans_avail_out, status_out ):
     scans_avail_out._obj.value = 0
+    debug('DIO64_In_Status(%d,%s,%s)', board, scans_avail_out, status_out )
 
   def DIO64_In_Read( self, board, buffer_out, scansToRead, status_in_out ):
-    pass
+    debug('DIO64_In_Read( %d,%s,%s,%s)',
+          board, buffer_out, scansToRead, status_in_out )
 
   def DIO64_In_Stop( self, board ):
-    pass
+    debug('DIO64_In_Stop(%d)', board )
 
   def DIO64_Out_ForceOutput( self, board, buf, mask ):
     buf_len = 0
@@ -38,7 +43,7 @@ class DIO64:
     log(DEBUG-1, 'DIO64_Out_ForceOutput(%d): %s', board, buf[0:buf_len])
 
   def DIO64_Out_GetInput( self, board, buf ):
-    pass
+    debug('DIO64_Out_GetInput(%d,%s)', board, buf )
 
   def DIO64_Out_Config( self, board, ticks, mask, len_mask, ignored_flags, clock,
                         trig_type_edge, trig_source,
@@ -46,28 +51,33 @@ class DIO64:
                         daq_clock_modulo, repetitions,
                         number_transitions, scan_rate ):
     self.out_len_mask = len_mask
+    debug('DIO64_Out_Config(%d,%d,%s,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+          board, ticks, mask[:], len_mask, ignored_flags, clock,
+          trig_type_edge, trig_source, stop_type_edge, stop_source,
+          daq_clock_modulo, repetitions, number_transitions, scan_rate )
 
   def DIO64_Out_Start( self, board ):
-    pass
+    debug('DIO64_Out_Start(%d)', board)
 
   def DIO64_Out_Status( self, board, scans_avail_out, status_out ):
     # we'll pretend to be _really_ generous
     scans_avail_out._obj.value = sys.maxint
     status_out._obj.time.value = sys.maxint
+    debug('DIO64_Out_Status(%d,%s,%s)', board, scans_avail_out, status_out )
 
   def DIO64_Out_Write( self, board, buf, buf_len, status_in_out ):
-    pass
+    debug('DIO64_Out_Write(%d,%s,%d,%s)', board, buf, buf_len, status_in_out)
 
   def DIO64_Out_Stop( self, board ):
-    pass
+    debug('DIO64_Out_Stop(%d)', board )
 
   def DIO64_SetAttr( self, board, attrId, value ):
-    log(DEBUG-1, 'DIO64_SetAttr(%d, %d, %d)', board, attrId, value)
+    debug('DIO64_SetAttr(%d, %d, %d)', board, attrId, value)
     self.attributes[attrId] = value
 
   def DIO64_GetAttr( self, board, attrId, value_out ):
     aid = attrId._obj.value
-    log(DEBUG-1, 'DIO64_GetAttr(%d, %d)', board, aid)
+    debug('DIO64_GetAttr(%d, %s(%d), %s)', board, attrId, aid, value_out)
 
     if aid in self.attributes:
       return self.attributes[aid]
