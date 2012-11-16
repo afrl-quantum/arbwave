ba2a55f58272c17b27225a77669b2715c5132e4c
Spencer E. Olson <olsonse@umich.edu>
2012-11-16 09:03:40 -0700

    improve optimize.test

--- src/gui/edit/optimize/test.py
+++ src/gui/edit/optimize/test.py
@@ -1,12 +1,15 @@
 # vim: ts=2:sw=2:tw=80:nowrap
 
+from numpy.random import rand
+
 var0 = 0.4
 var1 = 30
 var2 = dict( a = 100, b = 2000 )
 
 class func:
-  def __init__(self, avg=False):
+  def __init__(self, avg=False, Nextra=8):
     self.avg = avg
+    self.ne = Nextra
 
   def onstart(self):
     print 'nothing to get ready for!'
@@ -22,12 +25,19 @@ class func:
     zz1 = var2['b'] * 0.0001
     #f = ( sin((xx/40. + (yy/30.)**2))**2.0) * 100.0
     f = xx**2 + abs(yy-100) * abs(yy+100) + abs(zz0-100) + (zz1+50)**2
+    R = list(rand(self.ne))
     if self.avg:
       #return average([ f for i in xrange(10) ])
-      return average([ poisson( f ) for i in xrange(10) ])
+      return [average([ poisson( f ) for i in xrange(10) ])] + R
     else:
-      return f
+      return [f] + R
 
+  def extra_data_labels(self):
+    return ['rand'] * self.ne
+
+
+def get_globals():
+  return globals()
 
 import traceback, pprint
 from executor import Make
