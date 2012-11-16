630b0e77e138c19abb64cb27a185af9fe3ad3dc9
Spencer E. Olson <olsonse@umich.edu>
2012-11-16 13:38:54 -0700

    auto load config file upgraders

--- src/tools/compatibility/__init__.py
+++ src/tools/compatibility/__init__.py
@@ -1,3 +1,19 @@
 # vim: ts=2:sw=2:tw=80:nowrap
-import from_0_1_2
+import os
+
+def auto_load():
+  THISDIR = os.path.dirname( __file__ )
+  files = [ # all from_.py files without the .py extension
+    f[:-3]  for f in os.listdir( THISDIR )
+    if f.startswith('from_') and f.endswith('.py')
+  ]
+  try:
+    for f in files:
+      __import__(f, globals=globals(), locals=locals())
+  except Exception, e:
+    print "could not import converter '" + f + "'"
+    print e
+
+# auto load all converters
+auto_load()
 from register import conversion_path
