0999bdda9319d53916c15f81788268021d371c6e
Spencer E. Olson <olsonse@umich.edu>
2016-05-07 19:19:03 -0600

    Add tests for signal graphs and version graphs

---
+++ python/arbwave/tools/compatibility/tests/test_compatibility.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# vim: ts=2:sw=2:tw=80:nowrap
+
+import unittest, os
+from .. import conversion_path
+
+def get_conversion_path_str(_from, to):
+  path = conversion_path(_from, to)
+  return [ (f.__module__.rpartition('.')[-1], f.func_name) for f in path ]
+
+class Compatibility(unittest.TestCase):
+  short_path = [ ('from_0_1_2', 'to_0_1_3') ]
+
+  long_path = [
+    ('from_0_1_2', 'to_0_1_3'),
+    ('from_0_1_3', 'to_0_1_4'),
+    ('from_0_1_4', 'to_0_1_5'),
+    ('from_0_1_5', 'to_0_1_6'),
+    ('from_0_1_6', 'to_0_1_7'),
+    ('from_0_1_7', 'to_0_1_8'),
+  ]
+
+  def test_short_conversion_path(self):
+    path_str = get_conversion_path_str('0.1.2', '0.1.3')
+    self.assertEqual( self.short_path, path_str )
+
+  def test_long_conversion_path(self):
+    path_str = get_conversion_path_str('0.1.2', '0.1.8')
+    self.assertEqual( self.long_path, path_str )
