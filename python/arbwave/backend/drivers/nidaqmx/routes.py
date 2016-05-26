afrl-quantum/arbwave

commit 1722d4ceb47a2b4b3ee07ba1cdcbc8412c1885a2

add (ao/SampleClock,OnboardClock) -> ao/SampleClock route
This is  necessary to allow the new OnboardClock timing device to time the
analog output device with configure_timing_sample_clock.

python/arbwave/backend/drivers/nidaqmx/routes.py
@@ -33,6 +33,7 @@ PXI5= '{PXI_Trig{0..5}}'
 PXIi7 = 'PXI_Trig7'
 MTB = 'MasterTimebase'
 ao_SC = 'ao/SampleClock'
+ao_OC = ImplicitRoute( (ao_SC, NoDevTerminal('OnboardClock')) )
 ao_ST = 'ao/StartTrigger'
 ao_SCTB = 'ao/SampleClockTimebase'
 ai_SCTB = 'ai/SampleClockTimebase'
@@ -52,7 +53,7 @@ available = {
     ao_SC                 : { 'PFI5', (T6,R6) },
     ao_ST                 : { 'PFI6', (T6,R6) },
     '20MHzTimebase'       : { (Ti7,Ri7), ao_SCTB, 'Ctr{0,1}Source', MTB },
-    ao_SCTB               : { ao_SC },
+    ao_OC                 : { ao_SC }, # means OnboardClock --> ao_SC
     'Ctr0Out'             : { (T6,R6) },
     'Ctr0Gate'            : { 'PFI9', (T6,R6) },
     'Ctr0Source'          : { 'PFI8', (T6,R6) },
@@ -71,7 +72,7 @@ available = {
     ao_SC                 : { 'PFI5', (T5,PXI5),                   dio_SC },
     ao_ST                 : { 'PFI6', (T5,PXI5) },
     '20MHzTimebase'       : { (Ti7,PXIi7), ao_SCTB, 'Ctr{0,1}Source', MTB },
-    ao_SCTB               : { ao_SC },
+    ao_OC                 : { ao_SC }, # means OnboardClock --> ao_SC
     'Ctr0Out'             : { (T5,PXI5) },
     'Ctr0Gate'            : { 'PFI9', (T5,PXI5) },
     'Ctr0Source'          : { 'PFI8', (T5,PXI5) },
@@ -101,7 +102,7 @@ available = {
     'ao/PauseTrigger'     : { (T7,R7) },
     ai_CCTB               : { ai_CC },
     ai_SCTB               : { ai_SC, ai_CCTB },
-    ao_SCTB               : { ao_SC },
+    ao_OC                 : { ao_SC }, # means OnboardClock --> ao_SC
     'Ctr0Source'          : { P15, (T7,R7), 'Ctr1Gate', 'Ctr1Aux' },
     'Ctr1Source'          : { P15, (T7,R7), 'Ctr0Gate', 'Ctr0Aux' },
     'Ctr0Gate'            : { P15, (T7,R7), 'Ctr1Source', 'Ctr{0,1}Aux' },

commit d7a2f355163091d3afb0942b80f8dbcc5781248f

Fixes: 1722d4ceb4 again; detect need for */SampleClock-->OnboardClock
This certainly works for the analog output devices where the SampleClock is
actually identified as "OnboardClock", with no prefixes, when it is used as
the SampleClock source.  Not sure when this change will break--if NI has
some other way of identifying some sort of an internal sample clock for
some device, but does not call it OnboardClock.

python/arbwave/backend/drivers/nidaqmx/routes.py
@@ -50,7 +50,7 @@ available = {
     P9                    : { ao_sig, 'Ctr{0,1}{Gate,Source}', Ext },
     (T6,R6)               : { ao_sig, 'Ctr{0,1}{Out,Gate,Source}' },
     (Ti7,Ri7)             : {            ao_SCTB, 'Ctr{0,1}Source', MTB },
-    ao_OC                 : { ao_SC, 'PFI5', (T6,R6) },
+    ao_SC                 : { ao_SC, 'PFI5', (T6,R6) },
        # above allows OnboardClock --> ao_SC
     ao_ST                 : { 'PFI6', (T6,R6) },
     '20MHzTimebase'       : { (Ti7,Ri7), ao_SCTB, 'Ctr{0,1}Source', MTB },
@@ -69,7 +69,7 @@ available = {
     P9                    : { ao_sig, 'Ctr{0,1}{Gate,Source}', Ext },
     (T6,R6)               : { ao_sig, 'Ctr{0,1}{Out,Gate,Source}', dio_SC },
     (Ti7,Ri7)             : {              ao_SCTB, 'Ctr{0,1}Source', MTB },
-    ao_OC                 : { ao_SC, 'PFI5', (T6,R6),              dio_SC },
+    ao_SC                 : { ao_SC, 'PFI5', (T6,R6),              dio_SC },
        # above allows OnboardClock --> ao_SC
     ao_ST                 : { 'PFI6', (T6,R6) },
     '20MHzTimebase'       : { (Ti7,Ri7),   ao_SCTB, 'Ctr{0,1}Source', MTB },
@@ -88,7 +88,7 @@ available = {
     P9                    : { ao_sig, 'Ctr{0,1}{Gate,Source}', Ext },
     (T5,PXI5)             : { ao_sig, 'Ctr{0,1}{Out,Gate,Source}', dio_SC },
     (Ti7,PXIi7)           : {              ao_SCTB, 'Ctr{0,1}Source', MTB },
-    ao_OC                 : { ao_SC, 'PFI5', (T5,PXI5),            dio_SC },
+    ao_SC                 : { ao_SC, 'PFI5', (T5,PXI5),            dio_SC },
        # above allows OnboardClock --> ao_SC
     ao_ST                 : { 'PFI6', (T5,PXI5) },
     '20MHzTimebase'       : { (Ti7,PXIi7), ao_SCTB, 'Ctr{0,1}Source', MTB },
@@ -108,7 +108,7 @@ available = {
     'PFI{6..15}'          : {          ai_sig, ao_sig, dio_SC, 'Ctr{0,1}{Gate,Source,Aux,ArmStartTrigger,A,B,Z}', Ext },
     (T7,R7)               : { P15,     ai_sig, ao_sig, dio_SC, 'Ctr{0,1}{Gate,Source,Aux,ArmStartTrigger,A,B,Z}' },
     ai_SC                 : { P15, (T7,R7), dio_SC },
-    ao_OC                 : { ao_SC, P15, (T7,R7),     dio_SC },
+    ao_SC                 : { ao_SC, P15, (T7,R7),     dio_SC },
        # above allows OnboardClock --> ao_SC
     ai_CC                 : { P15, (T7,R7),            dio_SC },
     ai_ST                 : { P15, (T7,R7), ao_ST, 'Ctr{0,1}{Gate,Aux,ArmStartTrigger}' },

python/arbwave/backend/drivers/nidaqmx/task/task.py
@@ -90,6 +90,10 @@ class Task(Base):
             nearest_terminal( self.config['clock']['value'],
                               set(self.clock_sources),
                               signal_graph ) ]
+        if self.clock_terminal == self.format_ni_terminal_name('SampleClock'):
+          # this certainly works for NI analog devices.  Not sure when this
+          # would break.
+          self.clock_terminal = 'OnboardClock'
         do_rebuild = True
 
     if do_rebuild: