23740696728a8937bd2cf8c1ca7b688909f41519
Spencer E. Olson <olsonse@umich.edu>
2016-07-06 09:48:16 -0600

    removes new ctypes interface--will be used externally
    This patch breaks the functionality of the comedi backend.  The next change
    will restore it (to at least its previous not-completed state).

--- python/arbwave/backend/drivers/comedi/ctypes_comedi/__init__.py
+++
@@ -1,8 +0,0 @@
-# vim: ts=2:sw=2:tw=80:nowrap
-"""
-a ctypes wrapper for comedi that provides an interface that is compatible with
-the c-library (and for which the c-library manuals still work).
-"""
-
-from ctypes_comedi import *
-import extensions
