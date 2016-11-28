afrl-quantum/arbwave

commit 708c5ade76b70235dcc14ce53756d388318caf02

comedi: implement new distribution of finite_end_clock
Re-implements new tactic for determining whether a channel requires a
end-clock in non-continuous mode.  This also implements, for comedi, the
rule that drivers/channels ignore the last transition if arbwave has been
instructed to generate a end-clock for that particular channel.

This patch adds a lookup table that is going to need to be kept up-to-date
with respect to drivers/boards--this is because comedi does not currently
provide the means to identify subdevices/channels that require an extra
clock at the end of a sequence when in finite-generation mode.

python/arbwave/backend/drivers/comedi/subdevice/subdevice.py
@@ -17,6 +17,7 @@ from .....tools.cmp import cmpeps
 from .....tools import cached
 from ....device import Device as Base
 from .. import channels
+from . import capabilities
 
 
 command_test_errors = {
@@ -435,6 +436,11 @@ class Subdevice(Base):
         raise RuntimeError('comedi.get_min_period: get_cmd_timing_constraints failed')
       return scan_begin_min.value*unit.ns
 
+  @cached.property
+  def finite_end_clock(self):
+    C = capabilities.get(self.card.kernel, self.card.board, self.subdev_type)
+    return C['finite_end_clock']
+
   def set_waveforms(self, waveforms, clock_transitions, t_max, continuous):
     """
     Set up the hardware for waveform output.  This function does:
@@ -452,6 +458,15 @@ class Subdevice(Base):
     dt_clk = my_clock['dt']
     transitions = list( my_clock['transitions'] )
     transitions.sort()
+
+    if self.finite_end_clock and not continous:
+      # This subdevice requires an additional clock pulse at the end of the
+      # sequence in order for the hardware to properly notify the software of
+      # completion.  It is the responsibility of each driver to ensure that the
+      # last clock transitions is ignored if the driver has already indicated to
+      # arbwave that an extra clock pulse is required.
+      transitions = transitions[:-1]
+
 
 
     # 1. Set (non)continuous mode and number of samples per channel

python/arbwave/backend/drivers/comedi/channels/base.py
@@ -9,3 +9,23 @@ class Base(root_base):
     clock pulse) in units of seconds.
     """
     return self.device.get_min_period()
+
+  @property
+  def _finite_end_clock(self):
+    """
+    Identifies a channel as requiring a special extra clock pulse at the end of
+    the sequence in _non-continuous_ mode so that the hardware will recognize
+    the end of the sequence (and also thus notify the software).  Currently,
+    only NI hardware is known to require this.
+
+    * For this specialization (for comedi), we need to test each subdevice
+    * separately.  We'll use a lookup table to map (kernel,board) to known
+    * capabilities for devices.
+
+    !!!!NOTE!!!!
+    For any driver/channel that defines this requirement, an extra transition
+    will be generated by its clock in order to faciltate this last transition.
+    Thus, it is necessary that the driver/channel ignore the last transition
+    that is given to its clock source.
+    """
+    return self.device.finite_end_clock

python/arbwave/backend/drivers/comedi/subdevice/capabilities.py
@@ -1,0 +1,34 @@
+# vim: ts=2:sw=2:tw=80:nowrap
+"""
+Lookup table of known (kernel,board,subdev_type) tuples to capabilities for subdevices.
+The board_id and subdev_type elements of the tuple may be a wild card ('*').
+
+The capabilities that are cataloged currently are:
+  - finite_end_clock
+    This is an indicator of whether the particular subdevice requires an extra
+    clock at the end of the buffer sequence in order to signal the software that
+    the sequence has finished.
+"""
+
+table = {
+  ('ni_pcimio','*','ao') : dict(
+    finite_end_clock = True,
+  ),
+
+  # default entry
+  'default' : dict(
+    finite_end_clock = False,
+  ),
+}
+
+def get(kernel_module, board_name, subdev_type):
+  if   (kernel_module, board_name, subdev_type) in table:
+    return table.get((kernel_module, board_name, subdev_type))
+  elif (kernel_module, board_name, '*') in table:
+    return table.get((kernel_module, board_name, '*'))
+  elif (kernel_module, '*', subdev_type) in table:
+    return table.get((kernel_module, '*', subdev_type))
+  elif (kernel_module, '*', '*') in table:
+    return table.get((kernel_module, '*', '*'))
+
+  return table.get('default') # return the default

commit 3cce8f94a090c4ae015a2d87881ef6e78d45992a

Fixes mispelling of continuous
Fixes: 708c5ade76b70235dcc14ce53756d388318caf02 where continuous was
spelled wrong.

python/arbwave/backend/drivers/comedi/subdevice/subdevice.py
@@ -459,7 +459,7 @@ class Subdevice(Base):
     transitions = list( my_clock['transitions'] )
     transitions.sort()
 
-    if self.finite_end_clock and not continous:
+    if self.finite_end_clock and not continuous:
       # This subdevice requires an additional clock pulse at the end of the
       # sequence in order for the hardware to properly notify the software of
       # completion.  It is the responsibility of each driver to ensure that the