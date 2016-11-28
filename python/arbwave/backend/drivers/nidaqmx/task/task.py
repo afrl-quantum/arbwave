afrl-quantum/arbwave

commit 58d53944771fa0c57392947da23900f5b8195b9e

re-implement distribution of finite_end_clock capability/requirement
This patch pushes the requirement specification for finite_end_clock lower
into the drivers.  This patch also defines and implements the rule for a
driver/channel that requires an end-clock in order to complete a
non-continuous output sequence:  The driver/channel must ignore the last
transition given to it if it instructs arbwave to generate a final clock.

python/arbwave/backend/drivers/nidaqmx/task/task.py
@@ -17,6 +17,7 @@ class Task(Base):
   STATIC              = 0
   WAVEFORM_SINGLE     = 1
   WAVEFORM_CONTINUOUS = 2
+  finite_end_clock = True # whether this task requires end-clock (see analog.py)
 
   def __init__(self, driver, device):
     Base.__init__(self, name='{d}/{tt}'.format(d=device,tt=self.task_type))
@@ -216,6 +217,14 @@ class Task(Base):
     dt_clk = my_clock['dt']
     transitions = list( my_clock['transitions'] )
     transitions.sort()
+
+    if self.finite_end_clock and not continous:
+      # This task requires an additional clock pulse at the end of the sequence
+      # in order for the hardware to properly notify the software of completion.
+      # It is the responsibility of each driver to ensure that the last clock
+      # transitions is ignored if the driver has already indicated to arbwave
+      # that an extra clock pulse is required.
+      transitions = transitions[:-1]
 
     # 1.  Sample clock
     if continuous:

python/arbwave/backend/channels/base.py
@@ -44,6 +44,18 @@ class Base(object):
     return 0*unit.s
 
   def finite_mode_requires_end_clock(self):
+    """
+    Identifies a channel as requiring a special extra clock pulse at the end of
+    the sequence in _non-continuous_ mode so that the hardware will recognize
+    the end of the sequence (and also thus notify the software).  Currently,
+    only NI hardware is known to require this.
+
+    !!!!NOTE!!!!
+    For any driver/channel that defines this requirement, an extra transition
+    will be generated by its clock in order to faciltate this last transition.
+    Thus, it is necessary that the driver/channel ignore the last transition
+    that is given to its clock source when the output is not in continuous mode.
+    """
     return self._finite_end_clock
 
   def type(self):

python/arbwave/backend/drivers/nidaqmx/channels/base.py
@@ -9,3 +9,22 @@ class Base(root_base):
     clock pulse) in units of seconds.
     """
     return self.device.get_min_period()
+
+  @property
+  def _finite_end_clock(self):
+    """
+    Identifies a channel as requiring a special extra clock pulse at the end of
+    the sequence so that the hardware will recognize the end of the sequence
+    (and also thus notify the software).  Currently, only NI hardware is known
+    to require this.
+
+    !!!!NOTE!!!!
+    For any driver/channel that defines this requirement, an extra transition
+    will be generated by its clock in order to faciltate this last transition.
+    Thus, it is necessary that the driver/channel ignore the last transition
+    that is given to its clock source.
+
+    * Not yet clear whether this applies to only AO tasks or DO tasks as well.
+    * Thus, this info is provided by the task classes.
+    """
+    return self.device.finite_end_clock

python/arbwave/backend/drivers/nidaqmx/task/analog.py
@@ -10,6 +10,9 @@ class Analog(Base):
   task_type = 'ao'
   task_class = nidaqmx.AnalogOutputTask
 
+  # Refer to the NI documentation, DAQ-STC, Section 3.4.5.1 "Single-Buffer Mode"
+  finite_end_clock = True
+
 
   def add_channels(self):
     # populate the task with output channels and accumulate the data

python/arbwave/processor/engine/compute.py
@@ -430,7 +430,9 @@ class WaveformEvalulator:
       # has completed the waveform.
 
       for clk in self.finite_mode_end_clocks_required:
-        self.transitions[clk].add( max(self.transitions[clk]) + 1 )
+        end_clk_t = max(self.transitions[clk]) + 1
+        debug('add end clock for %s at %s', clk, end_clk_t)
+        self.transitions[clk].add(end_clk_t)
 
       # for simplicity, we just assume that the largest clock was needed.
       self.t_max += max( self.min_periods.viewvalues() )

commit 1d44fca2daf2b647f986b26ec36125f92427b949

Not all NIDAQmx tasks require end-clocks
Fixes: 58d53944771fa0c57392947da23900f5b8195b9e where it was intended that
only analog tasks be marked as requiring end-clocks.  This fixes this typo.

python/arbwave/backend/drivers/nidaqmx/task/task.py
@@ -17,7 +17,7 @@ class Task(Base):
   STATIC              = 0
   WAVEFORM_SINGLE     = 1
   WAVEFORM_CONTINUOUS = 2
-  finite_end_clock = True # whether this task requires end-clock (see analog.py)
+  finite_end_clock = False # whether this task requires end-clock (see analog.py)
 
   def __init__(self, driver, device):
     Base.__init__(self, name='{d}/{tt}'.format(d=device,tt=self.task_type))