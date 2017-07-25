991e20ecc82f4550331fc61642d8798adde821b3
Spencer E. Olson <olsonse@umich.edu>
2017-07-25 07:05:01 -0600

    Define U0 as last channel value regardless of whether sympy is available

--- python/arbwave/processor/engine/compute.py
+++ python/arbwave/processor/engine/compute.py
@@ -344,11 +344,11 @@ class WaveformEvalulator:
     # we're finally to the point to begin evaluating the value of the element
     locals['t'] = t
     locals['dt'] = dt
+    locals['U0'] = ci['last']
 
     if has_sympy:
       # insert symbols in case the user enters a sympy expression
       locals['x'] = self.x
-      locals['U0'] = ci['last']
       locals.setdefault('expr_steps', globals.get('expr_steps', 10))
       locals.setdefault('expr_err', globals.get('expr_err', 0.1))
       locals.setdefault('expr_fmt', globals.get('expr_fmt', 'uniform'))
