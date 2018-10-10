afrl-quantum/arbwave

commit d998ad16996d0d61b05a27b2524a9bb0e7cdc9c8

Add alpha implementation of bbb backend

python/arbwave/backend/drivers/bbb/device/dds.py
@@ -1,0 +1,170 @@
+# vim: ts=2:sw=2:tw=80:nowrap
+# -*- coding: utf-8 -*-
+"""
+Logical device driver for the BeagleBone Black using AFRL firmware/hardware.
+"""
+
+import copy
+import itertools
+from logging import debug
+import numpy as np
+from physical import units
+import time
+
+from .....tools.float_range import float_range
+from .....tools.dict import Dict
+from .....tools import cached
+from .. import channels
+from .base import Device as Base
+
+
+class Device(Base):
+  """
+  The Logical Device for a single instance of the BeagleBone Black DDS device
+  using AFRL firmware/hardware.
+  """
+
+
+  def __init__(self, *a, **kw):
+    super(Device,self).__init__(*a, **kw)
+    self.channels = [
+      channels.DDS('{}/{}'.format(self,i), self) for i in xrange(4)
+    ]
+    self.config = None
+
+
+  def close(self):
+    super(Device,self).close()
+    self.config = None
+
+
+  @cached.property
+  def possible_clock_sources(self):
+    return [
+      '{}/ClockIn'.format(self),
+      '{}/pru0/r30_0'.format(self.hostid),
+    ]
+
+
+  def get_routeable_backplane_signals(self):
+    return [
+      channels.Backplane('External/', ['{}/ClockIn'.format(self)]),
+    ]
+
+
+  def get_config_template(self):
+    """
+    Create the configuration template (the set of possible configuration
+    parameters and their allowed values).
+
+    :return: the configuration template
+    """
+
+    if self.proxy is None:
+      self.open()
+
+    D = Dict(self.proxy.get_sysclk())
+
+    config = {
+      'sysclk': {
+        'value': D.sysclk,
+        'type' : float,
+        'range': float_range(1e6, 500e6),
+      },
+      'has_crystal_refclk': {
+        'value': D.has_crystal_refclk,
+        'type' : bool,
+        'range': None,
+      },
+      'refclk': {
+        'value': D.refclk,
+        'type' : float,
+        'range': float_range(1e6, 100e6) if not D.has_crystal_refclk \
+            else float_range(20e6, 30e6),
+        'doreload' : True, # reload the range everytime we need it
+      },
+      'pll_chargepump': {
+        'value': D.charge_pump,
+        'type' : str,
+        'range': self.proxy.get_charge_pump_values(),
+      },
+      'clock': {
+        'value': '',
+        'type': str,
+        'range': self.possible_clock_sources,
+        'doreload' : True, # reload the range everytime we need it
+      },
+    }
+
+    if self.config is None:
+      # If this is not set yet, this is the first time accessing device
+      self.config = config
+    return config
+
+  def set_config(self, config):
+    """
+    Set the internal configuration for the board. This
+    does not include the sequence specification or the backplane routing.
+
+    :param config: the configuration dictionary to be applied, compare
+                   get_config_template()
+    """
+    debug('bbb.Device(%s).set_config(config=%s)', self, config)
+    valid_keys = set([
+      'sysclk', 'has_crystal_refclk', 'refclk', 'pll_chargepump', 'clock'
+    ])
+    assert set(config.keys()).issubset(valid_keys), \
+      'bbb.Device({}): Unknown configuration keys for AFRL/BeagleBone Black' \
+      .format(self)
+
+    if self.config is None:
+      # If this is not set yet, this is the first time accessing device
+      self.config = self.get_config_template()
+
+    if self.config == config:
+      return
+
+    if (self.config['sysclk'] != config['sysclk'] or
+        self.config['has_crystal_refclk'] != config['has_crystal_refclk'] or
+        self.config['refclk'] != config['refclk'] or
+        self.config['pll_chargepump'] != config['pll_chargepump']):
+      self.proxy.set_sysclk(config['refclk']['value'],
+                            config['sysclk']['value'],
+                            config['pll_chargepump']['value'],
+                            config['has_crystal_refclk']['value'])
+        
+
+    # We don't really have to respond to the clock setting (for now, no hardware
+    # to configure for this change)
+
+    # finally, keep a copy of the config given to us
+    self.config = copy.deepcopy(config)
+
+
+  def get_output_channels(self):
+    return self.channels
+
+
+  def set_waveforms(self, waveforms, clock_transitions, t_max, continuous):
+    """
+    Set the output waveforms for the AFRL/BeagleBone Black device.
+
+    :param waveforms:
+      a dict('channel': {<wfe-path>: (<encoding>, [(t1, val1), (t2, val2)])})
+                      (see gui/plotter/digital.py for format)
+                      where wfe-path means waveform element path, referring to
+                      the unique identifier of the specific waveform element a
+                      particular set of values refers to.
+    :param clock_transitions: a dict('channel': { 'dt': dt, 
+                              'transitions': iterable})
+                              (see processor/engine/compute.py for format)
+    :param t_max: the maximum duration of any channel
+    :param continuous: bool of continuous or single-shot mode
+    """
+    debug('bbb.Device(%s).set_waveforms(waveforms=%s, clock_transitions=%s, ' \
+          't_max=%s, continuous=%s)',
+          self, waveforms, clock_transitions, t_max, continuous)
+    if self.proxy:
+      self.proxy.set_waveforms(
+        waveforms, clock_transitions, t_max, continuous)
+    self.is_continuous = continuous

python/arbwave/backend/drivers/bbb/device/controller/dds.py
@@ -1,0 +1,220 @@
+#!/usr/bin/env python
+# vim: ts=2:sw=2:tw=80:nowrap
+# -*- coding: utf-8 -*-
+"""
+Remote device interface for the BeagleBone Black using AFRL firmware/hardware.
+"""
+
+
+if __name__ == '__main__':
+  import sys, argparse, socket
+  from os.path import join as path_join, dirname, pardir
+  sys.path.insert(0, path_join( dirname(__file__), *((pardir,)*5) ) )
+  import arbwave.drivers.bbb.device.controller.dds as dds
+
+  hostname = socket.gethostname()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--hostid', default=hostname,
+    help='Specify the hostid label for this BeagleBone Black [Default: {}]'
+         .format(hostname))
+  args = parser.parse_args()
+
+  dds.main(args.hostid)
+  sys.exit()
+
+def main(hostid):
+  import Pyro.core, Pyro.naming
+  Pyro.core.initServer()
+
+  # locate the NS
+  daemon = Pyro.core.Daemon()
+  try:
+    locator = Pyro.naming.NameServerLocator()
+    print 'searching for Naming Service...'
+    ns = locator.getNS()
+  except NamingError:
+    ns = None
+
+  if ns is not None:
+    print 'Could not find name server'
+    daemon.useNameServer(ns)
+
+    # make sure our namespace group exists
+    try: ns.createGroup(BBB_PYRO_GROUP)
+    except NamingError: pass
+
+  obj = Pyro.core.ObjBase()
+  device = dds.Device(hostid)
+  obj.delegateTo(device)
+  uri = daemon.connect(obj, device.objectId)
+  print 'my uri is: ', uri
+
+  try:
+    daemon.requestLoop()
+  except:
+    # try removing self from Pyro name server
+    try: ns.unregister(device.objectId)
+    except NamingError: pass
+
+
+
+
+import copy
+import itertools
+from logging import debug
+import numpy as np
+from physical import units
+import time
+
+import bbb
+
+from .....tools import cached
+from ....device import BBB_PYRO_GROUP, format_objectId
+from .. import channels
+from .base import Device as Base
+
+
+BBB_VERSION = 'bbb-0.1.0'
+
+
+class Device(Base):
+  """
+  The Logical Device for a single instance of the BeagleBone Black using AFRL
+  firmware/hardware.
+  """
+
+  def __init__(self, hostid):
+    super(Device,self).__init__(hostid, 'dds')
+
+    self.config = self.get_config_template()
+    self.is_continuous = False
+    self.set_output({}) # write the port values to the hardware
+
+    assert bbb.version.compatible(bbb.VERSION, BBB_VERSION), \
+      'AFRL/BeagleBone Black version is incompatible'
+
+    self.device = bbb.ad9959.Device()
+
+    try:
+      assert self.device.sw_fw_compatible, \
+      'AFRL/BeagleBone Black software and firmware are not compatible with ' \
+      'each other'
+    except:
+      self.close()
+      raise
+
+    # after this reset, the system clock will have to be setup
+    self.device.reset()
+
+
+  def close(self):
+    """
+    Final cleanup.
+    """
+    debug('bbb.Device(%s).close()', self)
+    self.device.close()
+
+
+  def set_sysclk(self, refclk, sysclk, charge_pump, has_crystal_refclk):
+    refclk_MHz = refclk / 1e6 # must be in MHz
+    sysclk_MHz = sysclk / 1e6 # must be in MHz
+    charge_pump = bbb.ad9959.regs.FR1.CHARGE_PUMP['_'+charge_pump]
+    self.device.set_sysclk(refclk,_MHz sysclk_MHz, charge_pump,
+                           has_crystal_refclk)
+
+
+  def get_sysclk(self):
+    D = self.device.get_sysclk()
+    D.sysclk = D.pop('sysclk_MHz') * 1e6
+    D.refclk = D.pop('refclk_MHz') * 1e6
+    # convert the charge_pump value into a nice string representation, but drop
+    # the '_' prefix
+    D.charge_pump = bbb.ad9959.regs.FR1.CHARGE_PUMP.reverse()[D.charge_pump][1:]
+    # convert this to standard dict to more easily get across Pyro boundaries
+    return dict(D)
+
+
+  def get_charge_pump_values(self):
+    """
+    Return a list of string representations for all possible PLL charge pump
+    values.
+    """
+    # remove the '_'_ prefix
+    return tuple(v[1:] for v in bbb.ad9959.regs.FR1.CHARGE_PUMP.iterkeys())
+
+
+  def set_output(self, values):
+    """
+    Immediately force the output on several channels; all others are
+    unchanged.
+
+    :param values: the channels to set. May be a dict of { <channel>: <value>},
+                   or a list of [ (<channel>, <value>), ...] tuples or something
+                   equivalently coercable to a dict
+    """
+    debug('bbb.Device(%s).set_output(values=%s)', self, values)
+    if not isinstance(values, dict):
+      values = dict(values)
+
+    ch_shift = dict(ch0=0, ch1=1, ch2=2, ch3=3)
+    for channel, val in values.iteritems():
+      self.device.set_frequency(val, ch_shift(channel))
+
+
+  def set_waveforms(self, waveforms, clock_transitions, t_max, continuous):
+    """
+    Set the output waveforms for the AFRL/BeagleBone Black device.
+
+    :param waveforms:
+      a dict('channel': {<wfe-path>: (<encoding>, [(t1, val1), (t2, val2)])})
+                      (see gui/plotter/digital.py for format)
+                      where wfe-path means waveform element path, referring to
+                      the unique identifier of the specific waveform element a
+                      particular set of values refers to.
+    :param clock_transitions: a dict('channel': { 'dt': dt, 
+                              'transitions': iterable})
+                              (see processor/engine/compute.py for format)
+    :param t_max: the maximum duration of any channel
+    :param continuous: bool of continuous or single-shot mode
+    """
+    debug('bbb.Device(%s).set_waveforms(waveforms=%s, clock_transitions=%s, ' \
+          't_max=%s, continuous=%s)',
+          self, waveforms, clock_transitions, t_max, continuous)
+    if self.connection:
+      #self.connection.set_waveforms(
+      #  waveforms, clock_transitions, t_max, continuous)
+      pass
+    self.is_continuous = continuous
+
+
+  def start(self):
+    """
+    Start the sequence: arm the board.
+    """
+    debug('bbb.Device(%s).start()', self)
+    self.device.exec_waveform(1 if not self.is_continuous else 0)
+
+
+  def wait(self):
+    """
+    Wait for the sequence to finish.
+    """
+    if self.is_continuous:
+      raise RuntimeError('cannot wait for continuous waveform to finish')
+
+    reps = self.device.waitfor_waveform()
+    debug('bbb.Device(%s).wait(): dds finished %d iterations', self, reps)
+
+
+  def stop(self):
+    """
+    Forcibly stop any running sequence.  For the DDS device, this means writing
+    a Null instruction to the RpMsg queue.
+    """
+    self.device.null_op()
+    reps = self.device.waitfor_waveform()
+    if reps is not None:
+      # For Arbwave, this should only be executed if in continuous mode
+      assert self.is_continuous, 'bbb.Device(%s): should be in continuous mode'
+      debug('bbb.Device(%s).wait(): dds finished %d iterations', self, reps)
+    self.is_continuous = False

SUPPORTED_HARDWARE
@@ -9,3 +9,6 @@ Supported Hardware:
       GxFpga for digital output using AFRL firmware (verified with GX3500)
       GxAo for analog output (not yet complete)
     Comedi
+    BeagleBone Black with AFRL firmware/hardware:
+      timing using PRU (not yet complete)
+      DDS using PRU + AFRL/DDS Cape

python/arbwave/backend/drivers/README
@@ -16,6 +16,7 @@ Currently assigned prefixes:
   vp     : Viewpoint
   comedi : Comedi
   gxt    : Marvin Test GxFpga using AFRL firmware (verified with GX3500)
+  bbb    : BeagleBone Black (with AFRL firmware/hardware)
 
 
 Common paths should use the same name regardless of which device they are on.

python/arbwave/backend/drivers/bbb/__init__.py
@@ -1,0 +1,228 @@
+# vim: ts=2:sw=2:tw=80:nowrap
+# -*- coding: utf-8 -*-
+"""
+Arbwave driver for BeagleBone Black using AFRL firmware/hardware for
+experimental control outputs.  Example outputs are arbitrary timing, digital
+output, and DDS frequency generation.
+"""
+
+from logging import info, error, warn, debug, log, DEBUG, INFO, root as rootlog
+from itertools import chain
+import Pyro.core
+import Pyro.naming
+
+from ....tools.path import collect_prefix
+from ...driver import Driver as Base
+from .device import BBB_PYRO_GROUP, create_device
+
+
+class Driver(Base):
+  prefix = 'bbb'
+  description = 'Driver for BeagleBone Black w/ AFRL firmware/hardware'
+  has_simulated_mode = True
+
+  getProxyForURI = staticmethod(Pyro.core.getProxyForURI)
+
+
+  def __init__(self, *a, **kw):
+    super(Driver,self).__init__(*a, **kw)
+
+    # dict: device path --> Device instance
+    self.devices      = dict() # only configured devices
+
+    # dict: device uri  --> Device instance
+    self.all_devices  = dict() # both configured and unconfigured
+
+    # dict: user-specified uri --> (objectId, device uri)
+    self.extra_uris   = dict()
+
+    if self.simulated:
+      import sim
+      self._ns = sim.NS()
+      self.getProxyForURI = self._ns.getProxyForURI
+
+    else:
+      # prepare to discover remote devices
+      Pyro.core.initClient()
+      try:
+        self._ns = Pyro.naming.NameServerLocator().getNS()
+      except Pyro.core.NamingError:
+        self._ns = None
+        info('bbb:  could not find Pyro name server')
+
+    if self._ns:
+      self.get_devices()
+      info('found %d bbb devices from nameserver', len(self.all_devices))
+
+
+  def close(self):
+    """
+    Close connection to each device.
+    """
+    while self.devices:
+      name, device = self.devices.popitem()
+      debug( 'closing connection to BeagleBone Black: %s', name )
+      device.close()
+      del device
+
+
+  def device_opened(self, device):
+    self.devices[str(device)] = device
+
+
+  def get_devices(self):
+    """
+    Return list of  devices.  This will return all known devices, whether
+    configured or not.
+
+    This list is built from all known device URIs, from the name server (if
+    available) and from the set of extra URIs.
+    """
+    # first all URIs from the name server
+    uris = dict() # uri --> objectId
+    if self._ns:
+      for name, typ in self._ns.list(BBB_PYRO_GROUP):
+        if typ != 1: continue # silently skip sub-groups
+        objectId = '{}.{}'.format(BBB_PYRO_GROUP,name)
+        uris[ str(self._ns.resolve(objectId)) ] = objectId
+
+    # add in all device uris derived from user-specified extra URIs
+    uris.update({ uri:oid for uri,oid in self.extra_uris.itervalues() })
+
+    # remove old uris
+    for uri in (set(self.all_devices) - set(uris)):
+      self.all_devices.pop(uri)
+
+    # now we create device objects for each URI, but do not configure/initialize
+    # them.  We do not create new objects if they are found either in
+    # self.devices, or in self.all_devices--we simply ensure the objects are
+    # listed in self.all_devices.  We also look in self.devices, in case a user
+    # has accidentally lost the URI for one device that is already in service.
+    active_devices = {d.uri : d for d in self.devices.itervalues()}
+    for uri, objectId in uris.iteritems():
+      if uri in self.all_devices:
+        continue
+
+      if uri in active_devices:
+        # copy over the forgotten object
+        self.all_devices[uri] = active_devices[uri]
+
+      # no prior device found, so create an un'opened' one
+      self.all_devices[uri] = create_device(self, uri, objectId)
+
+    return self.all_devices.values()
+
+
+  def set_extra_device_URIs(self, uri_list):
+    """
+    Add list of static URIs to the device discoverer.  Using this interface from
+    a global script may be necessary if a Pyro name server is not accessible.
+    """
+    # ensure these are plain string URIs
+    uri_list = set(str(uri) for uri in uri_list)
+
+    # remove old uris
+    for uri in (set(self.extra_uris) - uri_list):
+      self.extra_uris.pop(uri)
+
+    for uri in uri_list:
+      if uri in self.extra_uris:
+        # this device has already been added and loaded(?)
+        continue
+
+      # need to find device uris from user uris.  The only way I know how to do
+      # that is to make/use a connection.
+      p = self.getProxyForURI(uri) # makes
+      try:
+        objectId = p.get_objectId()       # uses
+        debug('found bbb::{} object at extra uri: {}', objectId, p.URI)
+      except Pyro.core.ProtocolError:
+        debug('cannot find bbb::{} object at extra uri: {}', objectId, p.URI)
+
+      # record user-uri  --> device uri
+      self.extra_uris[uri] = (objectId, str(p.URI))
+      p._release() # release this connection
+      del p
+
+
+  def get_output_channels(self):
+    return list(chain(*(
+      d.get_output_channels() for d in self.devices.itervalues()
+    )))
+
+
+  def get_timing_channels(self):
+    return list(chain(*(
+      d.get_timing_channels() for d in self.devices.itervalues()
+    )))
+
+
+  def get_routeable_backplane_signals(self):
+    return list(chain(*(
+      d.get_routeable_backplane_signals() for d in self.devices.itervalues()
+    )))
+
+
+  def open_required_devices(self, devnames):
+    unopened = (set(devnames) - set(self.devices.iterkeys()))
+    if unopened:
+      # devices configured but not open?; open them
+      name_to_device = { str(d):d for d in self.all_devices.values() }
+      for devname in unopened:
+        debug('bbb: reopening connection to %s', devname)
+        name_to_device[devname].open()
+
+
+  def set_device_config( self, config, channels, signal_graph ):
+    debug('bbb.set_device_config(%s, %s, %s)', config, channels, signal_graph)
+    # we need to separate channels first by device
+    # (configs are already naturally separated by device)
+    # in addition, we use collect_prefix to drop the 'bbb/DevX' part of the
+    # channel paths
+    chans = collect_prefix(channels, 0, 2, 2)
+
+    # devices not configured anymore; remove them
+    for devname in (set(self.devices.iterkeys()) - set(config.iterkeys())):
+      dev = self.devices.pop(devname)
+      dev.close()
+      debug('bbb: closed unused device: %s', devname)
+
+    self.open_required_devices(config.iterkeys())
+
+    for d,dev in self.devices.iteritems():
+      dev.set_config( config[d] )
+
+
+  def set_clocks( self, clocks ):
+    debug('bbb.set_clocks')
+
+    self.open_required_devices(clocks.iterkeys())
+
+    for devname, clks in clocks.iteritems():
+      self.devices[devname].set_clocks(clks)
+
+
+  def set_signals( self, signals ):
+    debug('bbb.set_signals(signals=%s)', signals)
+    signals = collect_prefix(signals, 0, 2, prefix_list=self.devices)
+
+    self.open_required_devices(signals.iterkeys())
+
+    for devname, sigs in signals.iteritems():
+      self.devices[devname].set_signals(sigs)
+
+
+  def set_static( self, analog, digital ):
+    debug('bbb.set_static')
+    D = collect_prefix(digital, 0, 2, 2)
+    for devname, data in D.iteritems():
+      self.devices[ devname ].set_output( data )
+
+
+  def set_waveforms( self, analog, digital, transitions, t_max, continuous ):
+    debug('bbb.set_waveforms')
+    D = collect_prefix(digital, 0, 2, 2)
+    C = collect_prefix(transitions, 0, 2, 2)
+    for d,dev in self.devices.iteritems():
+      if d in D or d in C:
+        dev.set_waveforms( D.get(d,{}), C.get(d,{}), t_max, continuous )

python/arbwave/backend/drivers/bbb/channels.py
@@ -1,0 +1,99 @@
+# vim: ts=2:sw=2:tw=80:nowrap
+# -*- coding: utf-8 -*-
+"""
+Channel definitions for the driver of the BeagleBone Black using AFRL
+firmware/hardware.
+
+No BBB outputs channels require padded timing, but rather operate on a
+(timestamp, output-value) basis, similar to the viewpoint and gx3500_dio
+drivers.
+"""
+
+from physical import unit
+import sys
+
+from ...channels import Analog     as ABase
+from ...channels import DDS        as DDSBase
+from ...channels import Digital    as DBase
+from ...channels import Timing     as TBase
+from ...channels import Backplane  as BBase
+
+class Analog(ABase):
+  """
+  *** Analog hardware/firmware not implemented yet ***
+  """
+  _padded_timing = False
+
+class DDS(DDSBase):
+  """
+  Frequency generation using the AFRL DDS cape.  This cape includes a AD9959 DDS
+  chip from Analog Devices.  There are four separate output channels on this
+  device.
+  """
+  _padded_timing = False
+  _capabilities = {'step', 'linear'}
+
+  def get_min_period(self):
+    """
+    Returns the minimum timing period in units of seconds.
+    *** This needs to be measured again and corrected ***
+    """
+    return 1*unit.us
+
+
+
+_bbb_digital_minimum_neighbor_period = 45*unit.ns
+"""
+The BeagleBone Black timing firmware _does_ have timing resolution of 5*ns, but
+any two transitions must be separated by at least 45*ns.  We may want to limit
+this to 50*ns instead of 45*ns to be consistent with the timing provided by
+viewpoint and gxt.
+"""
+
+class Digital(DBase):
+  """
+  Digital output provided by BeagleBone Black using the AFRL PRU firmware.
+  """
+  _padded_timing = False
+
+  def get_min_period(self):
+    """
+    Returns the minimum timing period in units of seconds.
+    """
+    return _bbb_digital_minimum_neighbor_period
+
+class Timing(TBase):
+  """
+  Timing channel specialization for using the digital outputs as aperiodic
+  clock devices.
+  """
+  aperiodic = True
+
+  def _divider(self):
+    return self.device.clocks[ str(self) ]['divider']['value']
+
+  def get_min_period(self):
+    """
+    Returns the minimum timing period (period between two rising edges of this
+    clock pulse) in units of seconds.
+
+    :return: the minimum period with physical.uinit.s
+    """
+    return _bbb_digital_minimum_neighbor_period * self._divider()
+
+  def get_config_template(self):
+    """
+    Get the config_dict template for this channel.
+
+    :return: a dict('setting-name': {'value':v, 'type':t, 'range':xrange})
+    """
+    return {
+      'divider' : {
+        'value' : 2,
+        'type'  : int,
+        'range' : xrange(2, sys.maxint),
+      }
+    }
+
+
+class Backplane(BBase): pass

python/arbwave/backend/drivers/bbb/device/__init__.py
@@ -1,0 +1,25 @@
+# vim: ts=2:sw=2:tw=80:nowrap
+
+from base import Device
+import dds
+#import timing
+
+BBB_PYRO_GROUP = ':bbb'
+
+type_to_device_klass_map = {
+  'dds' : dds.Device,
+#  'timing' : Timing,
+}
+
+def format_objectId(hostid, type):
+  return '{}.{}/{}'.format(BBB_PYRO_GROUP, hostid, type)
+
+def parse_objectId(objectId):
+  grp, _, devname = objectId.partition('.')
+  _, _, type = devname.partition('/')
+  return grp, devname, type
+
+def create_device(driver, uri, objectId):
+  grp, devname, type = parse_objectId(objectId)
+  assert grp == BBB_PYRO_GROUP, 'Non-bbb group found'
+  return type_to_device_klass_map[type](driver, uri, devname)

python/arbwave/backend/drivers/bbb/device/base.py
@@ -1,0 +1,161 @@
+# vim: ts=2:sw=2:tw=80:nowrap
+# -*- coding: utf-8 -*-
+"""
+Logical device driver for the BeagleBone Black using AFRL firmware/hardware.
+"""
+
+from logging import debug, info
+
+from .....version import version as arbwave_version
+from ....device import Device as Base
+
+
+class Device(Base):
+  """
+  The Logical Device for a single instance of the BeagleBone Black using AFRL
+  firmware/hardware.
+  """
+
+
+  def __init__(self, driver, uri, devname):
+    super(Device,self).__init__(name='{}/{}'.format(driver, devname))
+    self.hostid = devname.split('/')[0]
+    self.driver = driver
+    self.uri    = uri
+    self.proxy  = None
+
+
+  def __del__(self):
+    self.close()
+
+
+  def close(self):
+    if self.proxy is not None:
+      self.proxy._release()
+      del self.proxy
+      self.proxy = None
+
+
+  def open(self):
+    """
+    Create the proxy to the remote object and add the device to the drivers list
+    of devices.  This function is called when a device is added to the devices
+    tab of the configuration dialog.
+    """
+    if self.proxy:
+      raise RuntimeError('bbb.Device({}): Connection already opened'.format(self))
+
+    self.proxy = self.driver.getProxyForURI(self.uri)
+
+    ## test and assert version compatibility
+    if arbwave_version() != self.proxy.get_version():
+      self.close()
+      raise RuntimeError(
+        'bbb.Device({}): remote AFRL/BeagleBone Black ({}) Arbwave version is incompatible'
+        .format(self, self.uri)
+      )
+
+    # now tell the driver that this device is configured
+    self.driver.device_opened(self)
+
+    info('found BeagleBone Black+AFRL device: %s', self)
+
+
+  def set_config(self, config):
+    """
+    Set the internal configuration for the board. This
+    does not include the sequence specification or the backplane routing.
+
+    :param config: the configuration dictionary to be applied, compare
+                   get_config_template()
+    """
+    pass
+
+
+  def get_output_channels(self):
+    return []
+
+
+  def get_timing_channels(self):
+    return []
+
+
+  def get_routeable_backplane_signals(self):
+    return []
+
+
+  def set_clocks(self, clocks):
+    """
+    Configure output channels on devices from the particular driver from which
+    clock signals are generated.
+    """
+    raise RuntimeError(
+      'bbb.Device({}):  does not have clocks channels'.format(self))
+
+
+  def set_signals(self, signals):
+    """
+    Set the backplane routing info.
+
+    :param signals: a dict of { (src, dest) : config_dict} where src and
+                    dest are both paths
+    """
+    raise RuntimeError(
+      'bbb.Device({}):  does not have routeable signals'.format(self))
+
+
+  def set_output(self, values):
+    """
+    Immediately force the output on several channels; all others are
+    unchanged.
+
+    :param values: the channels to set. May be a dict of { <channel>: <value>},
+                   or a list of [ (<channel>, <value>), ...] tuples or something
+                   equivalently coercable to a dict
+    """
+    debug('bbb.Device(%s).set_output(values=%s)', self, values)
+    self.proxy.set_output( data )
+
+
+  def set_waveforms(self, waveforms, clock_transitions, t_max, continuous):
+    """
+    Set the output waveforms for the AFRL/BeagleBone Black device.
+
+    :param waveforms:
+      a dict('channel': {<wfe-path>: (<encoding>, [(t1, val1), (t2, val2)])})
+                      (see gui/plotter/digital.py for format)
+                      where wfe-path means waveform element path, referring to
+                      the unique identifier of the specific waveform element a
+                      particular set of values refers to.
+    :param clock_transitions: a dict('channel': { 'dt': dt, 
+                              'transitions': iterable})
+                              (see processor/engine/compute.py for format)
+    :param t_max: the maximum duration of any channel
+    :param continuous: bool of continuous or single-shot mode
+    """
+    raise RuntimeError(
+      'bbb.Device({}): does not implement waveforms'.format(self))
+
+
+  def start(self):
+    """
+    Start the sequence: arm the board.
+    """
+    debug('bbb.Device(%s).start()', self)
+    self.proxy.start()
+
+
+  def wait(self):
+    """
+    Wait for the sequence to finish.
+    """
+    debug('bbb.Device(%s).wait()', self)
+    self.proxy.wait()
+
+
+  def stop(self):
+    """
+    Forceably stop any running sequence.
+    """
+    debug('bbb.Device(%s).stop()', self)
+    self.proxy.stop()

commit f685bfb20505717b56727eb3fd524386151a8520

bbb: ensures self.config is a unique instance
Fixes: d998ad16996d0d61b05a27b2524a9bb0e7cdc9c8

The original code initialized self.config with the first return value of
self.get_config_template().  The result was that config items were
erroneously set outside of the driver framework.

python/arbwave/backend/drivers/bbb/device/dds.py
@@ -101,7 +101,7 @@ class Device(Base):
 
     if self.config is None:
       # If this is not set yet, this is the first time accessing device
-      self.config = config
+      self.config = copy.deepcopy(config)
     return config
 
   def set_config(self, config, channels, signal_graph):
@@ -135,7 +135,7 @@ class Device(Base):
 
     if self.config is None:
       # If this is not set yet, this is the first time accessing device
-      self.config = self.get_config_template()
+      self.get_config_template()
 
     if self.number_configured_channels != len(channels):
       self.number_configured_channels = len(channels)