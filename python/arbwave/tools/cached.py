2c23eb98d10ee240f06a2bd9d4acce4139f7deaf
Spencer E. Olson <olsonse@umich.edu>
2018-10-25 15:33:07 -0600

    Replace cached properties with single multi-faceted property
    Properties can how have time-to-live (ttl)
    threadlocking can also be specified with lock=True|False keyword

--- python/arbwave/tools/cached.py
+++ python/arbwave/tools/cached.py
@@ -1,49 +1,80 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Daniel Greenfeld'
-__email__ = 'pydanny@gmail.com'
-__version__ = '0.1.5'
-__license__ = 'BSD'
-
+# vim: ts=2:sw=2:tw=80:nowrap
+import time
 import threading
 
+inf = float('inf')
+
+__all__ = ['property']
+
+class _dummy_lock(object):
+  def __enter__(self, *a, **kw):
+    pass
+  def __exit__(self, *a, **kw):
+    pass
 
 class property(object):
   """
-  A property that is only computed once per instance and then replaces
-  itself with an ordinary attribute. Deleting the attribute resets the
-  property.
+  A multi-purpose property that can:
+    - use a cache and only re-evaluate after a specified time-to-live
+    - replace the property class with an ordinary attribute
+      (this can be reset by deleting the attribute from the instance)
+    - cause all access into the non-ordinary property be controlled by a thread
+      lock
 
-  Source: https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
+  Options:
+    ttl   : time-to-live for cached value [Default: float('inf')]
+            if this is float('inf'), the property will be replaced by an
+            ordinary value.
+    lock  : boolean value to indicate that any access to this property in its
+            functional form will be controlled via a thread lock.
   """
+  def __init__(self, *a, ttl=inf, lock=False):
+    self.ttl = ttl
+    self.lock = _dummy_lock()
+    if lock:
+      self.lock = threading.RLock()
 
-  def __init__(self, func):
-    self.__doc__ = getattr(func, '__doc__')
+    if a:
+      self(*a)
+
+  def __repr__(self):
+    return '<{}(ttl={}, lock={}) at {}>'.format(
+      property.__name__,
+      self.ttl,
+      True if not isinstance(self.lock, _dummy_lock) else False,
+      hex(id(self)),
+    )
+
+  def __call__(self, func):
     self.func = func
+    self.__doc__    = getattr(func, '__doc__')
+    self.__name__   = getattr(func, '__name__')
+    self.__module__ = getattr(func, '__module__')
+    return self
 
-  def __get__(self, obj, cls):
+  def __get__(self, obj, klass):
+    # when this is requested on a klass not an instance
     if obj is None:
       return self
-    value = obj.__dict__[self.func.__name__] = self.func(obj)
-    return value
 
-
-class threaded_property(property):
-  """
-  A property version for use in environments where multiple
-  threads might concurrently try to access the property.
-  """
-  def __init__(self, func):
-    super(threaded_property, self).__init__(func)
-    self.lock = threading.RLock()
-
-  def __get__(self, obj, cls):
     with self.lock:
-      # Double check if the value was computed before the lock was
-      # acquired.
-      prop_name = self.func.__name__
-      if prop_name in obj.__dict__:
-        return obj.__dict__[prop_name]
+      if self.ttl == inf:
+        # We replace ourselves with an ordinary attribute
+        value = self.func(obj)
+        obj.__dict__[self.__name__] = value
+        return value
 
-      # If not, do the calculation and release the lock.
-      return super(threaded_property, self).__get__(obj, cls)
+      if not hasattr(obj, '_cache'):
+        obj._cache = dict()
+
+      now = time.time()
+      value, last_update = obj._cache.get(self.__name__, (None,-1))
+
+      if self.ttl > 0 and now - last_update <= self.ttl:
+        # not outlived yet
+        return value
+
+      # re-evaluate and cache
+      value = self.func(obj)
+      obj._cache[self.__name__] = (value, now)
+      return value
