4f696d5bd14ca099c5dd4279059c1b6e54ef10ed
Spencer E. Olson <olsonse@umich.edu>
2018-12-05 13:43:52 -0700

    examples: minor updates to match current save file output
    These updates are not done for compatibility (because the files were
    already compatible with the current version), but rather to simply look
    more like the files that get saved currently.  This change also updates the
    claimed version in the files to 1.1.0, to reflect the up-coming demarkation
    of Python3 conversion.

--- examples/simple_config.py
+++ examples/simple_config.py
@@ -1,7 +1,66 @@
+hosts = \
+{'__default__': 'local', 'local': 'localhost'}
+
+devices = \
+{'ni/Dev1/ao': {'clock': {'type': str, 'value': 'vp/Dev0/A/13'},
+                'clock-settings': {'Timebase': {'clock': {'type': str,
+                                                          'value': ''}},
+                                   'edge': {'type': str, 'value': 'rising'},
+                                   'mode': {'type': str,
+                                            'value': 'continuous'}},
+                'default-voltage-range': {'maximum': {'type': float,
+                                                      'value': 10.0},
+                                          'minimum': {'type': float,
+                                                      'value': -10.0}},
+                'trigger': {'edge': {'type': str, 'value': 'rising'},
+                            'enable': {'type': bool, 'value': False},
+                            'source': {'type': str, 'value': ''}},
+                'use-only-onboard-memory': {'type': bool, 'value': True}},
+ 'vp/Dev0': {'clock': {'type': str, 'value': 'vp/Dev0/Internal_XO'},
+             'in': {'daq_clock_modulo': {'type': int, 'value': 0},
+                    'divider': {'type': int, 'value': 0},
+                    'stop': {'type': int, 'value': 0},
+                    'stop_edge': {'type': int, 'value': 0},
+                    'trig_edge': {'type': int, 'value': 0},
+                    'trig_source': {'type': int, 'value': 0},
+                    'trig_type': {'type': int, 'value': 4}},
+             'out': {'daq_clock_modulo': {'type': int, 'value': 0},
+                     'divider': {'type': int, 'value': 0},
+                     'stop': {'type': int, 'value': 0},
+                     'stop_edge': {'type': int, 'value': 0},
+                     'trig_edge': {'type': int, 'value': 0},
+                     'trig_source': {'type': int, 'value': 0},
+                     'trig_type': {'type': int, 'value': 4}}}}
+
+clocks = \
+{'vp/Dev0/A/13': {'divider': {'type': int, 'value': 2}},
+ 'vp/Dev0/Internal_XO': {'scan_rate': {'type': float, 'value': 20000000.0}}}
+
+global_script = \
+"""# This script sets global variables and/or functions.
+# All other scripts and processing will be done in this context.
+from physical.unit import *
+from physical.constant import *
+from physical import unit
+import Arbwave
+
+expr_steps = 40
+
+class SimpleRun(Arbwave.Runnable):
+	def run(self):
+		Arbwave.update()
+		# if possible, the run function should return a value indicative of the
+		# performance of the particular run.  Below is just a "random" example.
+		import random
+		return random.randint(0,100)
+
+Arbwave.add_runnable( 'Simple', SimpleRun() )
+"""
+
 channels = \
-{'Camera Trigger': {'device': 'Digital/vp/Dev0/A/0',
+{'Camera Trigger': {'device': 'digital/vp/Dev0/A/0',
                     'enable': True,
-                    'interp_order': 0,
+                    'interp_order': 1,
                     'interp_smoothing': 0.0,
                     'offset': None,
                     'order': 0,
@@ -10,7 +69,7 @@ channels = \
                     'scaling': None,
                     'units': None,
                     'value': 'False'},
- 'ao0': {'device': 'Analog/ni/Dev1/ao0',
+ 'ao0': {'device': 'analog/ni/Dev1/ao0',
          'enable': True,
          'interp_order': 1,
          'interp_smoothing': 0.0,
@@ -21,7 +80,7 @@ channels = \
          'scaling': [('0', '0'), ('10', '10')],
          'units': 'V',
          'value': '10*V'},
- 'ao1': {'device': 'Analog/ni/Dev1/ao1',
+ 'ao1': {'device': 'analog/ni/Dev1/ao1',
          'enable': True,
          'interp_order': 1,
          'interp_smoothing': 0.0,
@@ -56,71 +115,12 @@ waveforms = \
                             'time': 't',
                             'value': 'U0 + (-2*V - U0)*x**0.5'}]}}
 
-clocks = \
-{'vp/Dev0/A/13': {'divider': {'type': int, 'value': 2}},
- 'vp/Dev0/Internal_XO': {'scan_rate': {'type': float, 'value': 20000000.0}}}
-
-hosts = \
-{'__default__': 'local', 'local': 'localhost'}
-
-version = \
-'1.0.0'
-
 signals = \
 {('vp/Dev0/A/13', 'TRIG/1'): {'invert': False}}
 
+version = \
+'1.1.0'
+
 runnable_settings = \
 {}
-
-global_script = \
-"""# This script sets global variables and/or functions.
-# All other scripts and processing will be done in this context.
-from physical.unit import *
-from physical.constant import *
-from physical import unit
-import Arbwave
-
-expr_steps = 40
-
-class SimpleRun(Arbwave.Runnable):
-	def run(self):
-		Arbwave.update()
-		# if possible, the run function should return a value indicative of the
-		# performance of the particular run.  Below is just a "random" example.
-		import random
-		return random.randint(0,100)
-
-Arbwave.add_runnable( 'Simple', SimpleRun() )
-"""
-
-devices = \
-{'ni/Dev1/ao': {'clock': {'type': str, 'value': 'vp/Dev0/A/13'},
-                'clock-settings': {'Timebase': {'clock': {'type': str,
-                                                          'value': ''}},
-                                   'edge': {'type': str, 'value': 'rising'},
-                                   'mode': {'type': str,
-                                            'value': 'continuous'}},
-                'default-voltage-range': {'maximum': {'type': float,
-                                                      'value': 10.0},
-                                          'minimum': {'type': float,
-                                                      'value': -10.0}},
-                'trigger': {'edge': {'type': str, 'value': 'rising'},
-                            'enable': {'type': bool, 'value': False},
-                            'source': {'type': str, 'value': ''}},
-                'use-only-onboard-memory': {'type': bool, 'value': True}},
- 'vp/Dev0': {'clock': {'type': str, 'value': 'vp/Dev0/Internal_XO'},
-             'in': {'daq_clock_modulo': {'type': int, 'value': 0},
-                    'divider': {'type': int, 'value': 0},
-                    'stop': {'type': int, 'value': 0},
-                    'stop_edge': {'type': int, 'value': 0},
-                    'trig_edge': {'type': int, 'value': 0},
-                    'trig_source': {'type': int, 'value': 0},
-                    'trig_type': {'type': int, 'value': 4}},
-             'out': {'daq_clock_modulo': {'type': int, 'value': 0},
-                     'divider': {'type': int, 'value': 0},
-                     'stop': {'type': int, 'value': 0},
-                     'stop_edge': {'type': int, 'value': 0},
-                     'trig_edge': {'type': int, 'value': 0},
-                     'trig_source': {'type': int, 'value': 0},
-                     'trig_type': {'type': int, 'value': 4}}}}
 
--- examples/simple_ni_only_config.py
+++ examples/simple_ni_only_config.py
@@ -1,5 +1,53 @@
+hosts = \
+{'__default__': 'local', 'local': 'localhost'}
+
+devices = \
+{'ni/Dev1/ao': {'clock': {'type': str, 'value': 'ni/Dev1/ao/SampleClock'},
+                'clock-settings': {'Timebase': {'clock': {'type': str,
+                                                          'value': ''}},
+                                   'edge': {'type': str, 'value': 'rising'},
+                                   'mode': {'type': str,
+                                            'value': 'continuous'}},
+                'default-voltage-range': {'maximum': {'type': float,
+                                                      'value': 10.0},
+                                          'minimum': {'type': float,
+                                                      'value': -10.0}},
+                'trigger': {'edge': {'type': str, 'value': 'rising'},
+                            'enable': {'type': bool, 'value': False},
+                            'source': {'type': str, 'value': ''}},
+                'use-only-onboard-memory': {'type': bool, 'value': True}},
+ 'ni/Dev1/do': {'clock': {'type': str, 'value': 'ni/Dev1/ao/SampleClock'},
+                'clock-settings': {'Timebase': {'clock': {'type': str,
+                                                          'value': ''}},
+                                   'edge': {'type': str, 'value': 'rising'},
+                                   'mode': {'type': str,
+                                            'value': 'continuous'}},
+                'use-only-onboard-memory': {'type': bool, 'value': True}}}
+
+clocks = \
+{'ni/Dev1/ao/SampleClock': {'rate': {'type': float, 'value': 2000.0}}}
+
+global_script = \
+"""# This script sets global variables and/or functions.
+# All other scripts and processing will be done in this context.
+from physical.unit import *
+from physical.constant import *
+from physical import unit
+import Arbwave
+
+class SimpleRun(Arbwave.Runnable):
+	def run(self):
+		Arbwave.update()
+		# if possible, the run function should return a value indicative of the
+		# performance of the particular run.  Below is just a "random" example.
+		import random
+		return random.randint(0,100)
+
+Arbwave.add_runnable( 'Simple', SimpleRun() )
+"""
+
 channels = \
-{'ao0': {'device': 'Analog/ni/Dev1/ao0',
+{'ao0': {'device': 'analog/ni/Dev1/ao0',
          'enable': True,
          'interp_order': 1,
          'interp_smoothing': 0.0,
@@ -10,7 +58,7 @@ channels = \
          'scaling': [('-10', '-10'), ('10', '10')],
          'units': 'V',
          'value': '1*V'},
- 'do0': {'device': 'Digital/ni/Dev1/port0/line0',
+ 'do0': {'device': 'digital/ni/Dev1/port0/line0',
          'enable': True,
          'interp_order': 0,
          'interp_smoothing': 0.0,
@@ -67,63 +115,15 @@ waveforms = \
                             'script': None,
                             'time': 't'}]}}
 
-clocks = \
-{'ni/Dev1/ao/SampleClock': {'rate': {'type': float, 'value': 2000.0}}}
-
-hosts = \
-{'__default__': 'local', 'local': 'localhost'}
-
-version = \
-'1.0.0'
-
 signals = \
 {}
+
+version = \
+'1.1.0'
 
 runnable_settings = \
 {'Simple:  Loop': {'parameters': [{'enable': True,
                                    'isglobal': False,
                                    'iterable': 'range(0,10,2)',
                                    'name': 'i'}]}}
-
-global_script = \
-"""# This script sets global variables and/or functions.
-# All other scripts and processing will be done in this context.
-from physical.unit import *
-from physical.constant import *
-from physical import unit
-import Arbwave
-
-class SimpleRun(Arbwave.Runnable):
-	def run(self):
-		Arbwave.update()
-		# if possible, the run function should return a value indicative of the
-		# performance of the particular run.  Below is just a "random" example.
-		import random
-		return random.randint(0,100)
-
-Arbwave.add_runnable( 'Simple', SimpleRun() )
-"""
-
-devices = \
-{'ni/Dev1/ao': {'clock': {'type': str, 'value': 'ni/Dev1/ao/SampleClock'},
-                'clock-settings': {'Timebase': {'clock': {'type': str,
-                                                          'value': ''}},
-                                   'edge': {'type': str, 'value': 'rising'},
-                                   'mode': {'type': str,
-                                            'value': 'continuous'}},
-                'default-voltage-range': {'maximum': {'type': float,
-                                                      'value': 10.0},
-                                          'minimum': {'type': float,
-                                                      'value': -10.0}},
-                'trigger': {'edge': {'type': str, 'value': 'rising'},
-                            'enable': {'type': bool, 'value': False},
-                            'source': {'type': str, 'value': ''}},
-                'use-only-onboard-memory': {'type': bool, 'value': True}},
- 'ni/Dev1/do': {'clock': {'type': str, 'value': 'ni/Dev1/ao/SampleClock'},
-                'clock-settings': {'Timebase': {'clock': {'type': str,
-                                                          'value': ''}},
-                                   'edge': {'type': str, 'value': 'rising'},
-                                   'mode': {'type': str,
-                                            'value': 'continuous'}},
-                'use-only-onboard-memory': {'type': bool, 'value': True}}}
 
