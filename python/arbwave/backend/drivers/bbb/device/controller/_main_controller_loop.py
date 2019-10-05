909ad3885de8a0a3c9da2d80d62a9a9a727ccef1
Spencer E. Olson <olsonse@umich.edu>
2019-10-05 15:22:11 -0600

    bbb:  keep arbwave-bbb-[dds,timing]_controller NS-registered
    This patch adds a thread to monitor the Pyro4 nameserver (if found at
    start-up) to ensure that the dds/timing controllers remain registered.
    This change allows the Pyro4 nameserver to be as flakey as sometimes seems
    to be without having controllers permenantly drop off the registered Pyro4
    services list.

--- python/arbwave/backend/drivers/bbb/device/controller/_main_controller_loop.py
+++ python/arbwave/backend/drivers/bbb/device/controller/_main_controller_loop.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # vim: ts=2:sw=2:tw=80:nowrap
 # -*- coding: utf-8 -*-
 """
@@ -6,7 +6,7 @@ This file is not really for a normal package import
 """
 
 from .bbb_pyro import BBB_PYRO_GROUP, format_objectId
-import sys
+import sys, time, threading
 import Pyro4
 
 def main(klass, pyro_port=0):
@@ -23,6 +23,11 @@ def main(klass, pyro_port=0):
   parser.add_argument('--ns', metavar='NAMESERVER[:PORT]',
     help='Specify Pyro nameserver address (in case it cannot be reached by UDP '
          'broadcasts).')
+  parser.add_argument('--ns-probe', metavar='PERIOD', default=300, type=float,
+    help='The period (in seconds) between probing the nameserver to test if '
+         'this daemon is still registered correctly.  If this daemon is not '
+         'still registered, we will re-register it.  To disable this probe, '
+         'specify the value of --ns-probe as `inf`.')
   parser.add_argument('--bind', metavar='ADDRESS[:PORT]', default='0.0.0.0',
     help='Specify local interface to bind to')
   parser.add_argument('--nop', action='store_true',
@@ -63,13 +68,65 @@ def main(klass, pyro_port=0):
   uri.host = args.hostid
 
   if ns:
-    ns.register(device.objectId, uri, safe=not args.replace)
+    class NSMonitor(threading.Thread):
+      def __init__(self, objectId, uri, safe, ns, period):
+        super(NSMonitor, self).__init__()
+        self.objectId = objectId
+        self.uri = uri
+        self.safe = safe
+        self.ns = ns
+        self.period = period
+        self.last_test = -float('inf')
+        self.go = True
+
+      def stop(self):
+        self.go = False
+
+      def _regerr(self):
+        print('could not re-register', self.objectId, 'with nameserver.\n'
+              'Will try again in another', self.period, 'seconds')
+
+      def run(self):
+        while self.go:
+          now = time.time()
+          if now - self.last_test > self.period:
+            try:
+              p = self.ns.lookup(self.objectId)
+              if str(p) != self.uri:
+                raise Pyro4.errors.NamingError('incorrect URI')
+            except (Pyro4.errors.NamingError, Pyro4.errors.ConnectionClosedError):
+              try:
+                self.ns.register(self.objectId, self.uri, self.safe)
+                print('re-registered ', self.objectId, 'with nameserver')
+              except Exception as e:
+                if not isinstance(e, Pyro4.errors.CommunicationError):
+                  print('Unexpected exception in Pyro4 comms: ', e)
+                self._regerr()
+            except Exception as e:
+              if not isinstance(e, Pyro4.errors.CommunicationError):
+                print('Unexpected exception in Pyro4 comms: ', e)
+              self._regerr()
+
+            self.last_test = now
+
+          if self.period == float('inf'):
+            # no need to continue
+            return
+
+          time.sleep(self.period)
+
+    nsmonitor = NSMonitor(device.objectId, uri, not args.replace, ns,
+                          args.ns_probe)
+    nsmonitor.start()
+
   print('my uri is: ', uri)
 
   try:
     daemon.requestLoop()
   except:
     if ns:
+      nsmonitor.stop()
+      nsmonitor.join()
       # try removing self from Pyro name server
       try: ns.remove(device.objectId)
       except Pyro4.errors.NamingError: pass
